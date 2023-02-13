9244f8a7af631375b811a299d764023a4bf5612c
timas <cerbin20@gmail.com>
2023-02-13 01:57:25 +0100

    initial

--- leson 11/Leson 11 ex 1.py
+++ leson 11/Leson 11 ex 1.py
@@ -1,3 +1,8 @@
+# Write a script that creates a new output file called myfile.txt and writes the string
+# "Hello file world!" in it. Then write another script that opens myfile.txt, and reads and prints its contents.
+# Run your two scripts from the system command line.
+
+
 hello = "Hello file world!\n"
 with open("myfile.txt", "w") as file_object:
     file_object.write(hello)
