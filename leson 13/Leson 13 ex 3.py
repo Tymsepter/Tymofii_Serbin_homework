a93703c28b29ce40c9bb0f5d338088b897e2963b
timas <cerbin20@gmail.com>
2023-02-19 09:02:49 +0100

    initial

--- leson 13/Leson 13 ex 3.py
+++ leson 13/Leson 13 ex 3.py
@@ -21,3 +21,12 @@ assert choose_func(nums1, square_nums, remove_negatives) == [1, 4, 9, 16, 25]
 
 assert choose_func(nums2, square_nums, remove_negatives) == [1, 3, 5]
 
+
+def choose_func(nums: list, func1, func2):
+    if all(num > 0 for num in nums):
+        return func1(nums)
+    else:
+        return func2(nums)
+
+
+print(choose_func())
