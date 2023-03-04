131f54d7de6ff1e9957cb25816ee9917299f2532
timas <cerbin20@gmail.com>
2023-03-04 12:01:57 +0100

    initial

---
+++ Leson 15/Leson 15 ex 2.py
@@ -0,0 +1,12 @@
+class Dog:
+    age_factor = 7
+
+    def __init__(self, age):
+        self.age = age
+
+    def human_age(self):
+        return self.age * self.age_factor
+
+
+dog1 = Dog(3)
+print(dog1.human_age())
