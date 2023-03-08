27ff8ea4423fad216922f7b55096b17a70b018ee
timas <cerbin20@gmail.com>
2023-03-08 02:02:36 +0100

    initial

--- Leson 16/Leson 16 ex 2.py
+++ Leson 16/Leson 16 ex 2.py
@@ -4,12 +4,12 @@ math = Mathematician()
 
 nums = [1, 2, 3, 4, 5]
 squares = math.square_nums(nums)
-print(squares)  # [1, 4, 9, 16, 25]
+print(squares)
 
 nums = [1, -2, 3, -4, 5]
 negatives = math.remove_positives(nums)
-print(negatives)  # [-2, -4]
+print(negatives)
 
 years = [2000, 2001, 2004, 2005, 2008, 2010]
 leap_years = math.filter_leaps(years)
-print(leap_years)  # [2000, 2004, 2008]
+print(leap_years)
