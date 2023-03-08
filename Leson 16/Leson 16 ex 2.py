7041f8d8587d7683d20462495ea3dc095ab3d303
timas <cerbin20@gmail.com>
2023-03-08 02:12:11 +0100

    initial

--- Leson 16/Leson 16 ex 2.py
+++ Leson 16/Leson 16 ex 2.py
@@ -7,8 +7,8 @@ squares = math.square_nums(nums)
 print(squares)
 
 nums = [1, -2, 3, -4, 5]
-negatives = math.remove_positives(nums)
-print(negatives)
+only_negatives = math.remove_positives(nums)
+print(only_negatives)
 
 years = [2000, 2001, 2004, 2005, 2008, 2010]
 leap_years = math.filter_leaps(years)
