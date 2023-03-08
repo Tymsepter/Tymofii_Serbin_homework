351e568d64d6b7ca15110b9537d72d17b5929e23
timas <cerbin20@gmail.com>
2023-03-08 01:55:48 +0100

    initial

---
+++ Leson 16/mathematican.py
@@ -0,0 +1,9 @@
+class Mathematician:
+    def square_nums(self, nums):
+        return [num ** 2 for num in nums]
+
+    def remove_positives(self, nums):
+        return [num for num in nums if num <= 0]
+
+    def filter_leaps(self, years):
+        return [year for year in years if years % 4 == 0 and (year % 100 != 0 or year % 400 == 0)]
