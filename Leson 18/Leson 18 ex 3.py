3b15ab4237ec53e0994d4b5c2f73eac833e3f997
timas <cerbin20@gmail.com>
2023-04-06 18:57:14 +0200

    initial

---
+++ Leson 18/Leson 18 ex 3.py
@@ -0,0 +1,64 @@
+from functools import wraps
+
+
+class TypeDecorators:
+    @staticmethod
+    def to_int(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            try:
+                return int(result)
+            except (ValueError, TypeError):
+                return result
+
+        return wrapper
+
+    @staticmethod
+    def to_str(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            try:
+                return str(result)
+            except (ValueError, TypeError):
+                return result
+
+        return wrapper
+
+    @staticmethod
+    def to_bool(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if isinstance(result, str):
+                result = result.lower()
+            return result in [True, 1, '1', 'true', 'yes', 'on']
+
+        return wrapper
+
+    @staticmethod
+    def to_float(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            try:
+                return float(result)
+            except (ValueError, TypeError):
+                return result
+
+        return wrapper
+
+
+@TypeDecorators.to_int
+def do_nothing(string: str):
+    return string
+
+
+@TypeDecorators.to_bool
+def do_something(string: str):
+    return string
+
+
+assert do_nothing('25') == 25
+assert do_something('True') is True
